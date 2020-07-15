e791dcfeb3066621ae141025a5beca1638f4618d
peter7231 <peter7231@163.com>
2020-07-15 10:41:05 +0800

    提交测试代码

---
+++ cloud-provider-payment8001/src/main/java/com/liupan/springcloud/controller/PaymentController.java
@@ -0,0 +1,45 @@
+package com.liupan.springcloud.controller;
+
+import com.liupan.springcloud.entites.CommonResult;
+import com.liupan.springcloud.entites.Payment;
+import com.liupan.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class PaymentController
+{
+
+    @Resource
+    private PaymentService paymentService;
+
+    @PostMapping(value = "/payment/create")
+    public CommonResult create(Payment payment)
+    {
+        int result = paymentService.create(payment);
+        log.info("----插入结果："+result);
+        if(result>0){
+            return new CommonResult(200,"插入数据成功",result);
+        }else{
+            return new CommonResult(200,"插入数据失败");
+        }
+    }
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id)
+    {
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("----查询结果："+payment);
+        if(payment != null){
+            return new CommonResult(200,"查询成功",payment);
+        }else{
+            return new CommonResult(200,"查询失败");
+        }
+    }
+}
---
+++ cloud-provider-payment8001/src/main/resources/init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `payment` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `serial` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
